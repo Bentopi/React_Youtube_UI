361e7df23ac3c188de9c3dd3365a26d98589f7f1
Brian Simmons <bsimmonsn@gmail.com>
2017-09-10 14:30:07 -0500

    Revert "Trying out Yeoman to eventually deploy with kubernetes"
    This reverts commit 3545cb69438d8b432b3d331f047986a82b4c17f1.

--- server.js
+++ server.js
@@ -1,36 +1,13 @@
-/*eslint no-console:0 */
-'use strict';
-require('core-js/fn/object/assign');
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
-const config = require('./webpack.config');
-const open = require('open');
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
 
-/**
- * Flag indicating whether webpack compiled for the first time.
- * @type {boolean}
- */
-let isInitialCompilation = true;
+app.use(express.static(__dirname));
 
-const compiler = webpack(config);
-
-new WebpackDevServer(compiler, config.devServer)
-.listen(config.port, 'localhost', (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log('Listening at localhost:' + config.port);
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
-compiler.plugin('done', () => {
-  if (isInitialCompilation) {
-    // Ensures that we log after webpack printed its stats (is there a better way?)
-    setTimeout(() => {
-      console.log('\n✓ The bundle is now ready for serving!\n');
-      console.log('  Open in iframe mode:\t\x1b[33m%s\x1b[0m',  'http://localhost:' + config.port + '/webpack-dev-server/');
-      console.log('  Open in inline mode:\t\x1b[33m%s\x1b[0m', 'http://localhost:' + config.port + '/\n');
-      console.log('  \x1b[33mHMR is active\x1b[0m. The bundle will automatically rebuild and live-update on changes.')
-    }, 350);
-  }
-  isInitialCompilation = false;
-});
+app.listen(port);
+console.log('Server started');
