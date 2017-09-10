3545cb69438d8b432b3d331f047986a82b4c17f1
Brian Simmons <bsimmonsn@gmail.com>
2017-09-10 14:17:52 -0500

    Trying out Yeoman to eventually deploy with kubernetes

--- src/index.js
+++ src/index.js
@@ -1,46 +1,7 @@
-import _ from 'lodash';
-import React, { Component } from 'react';
+import 'core-js/fn/object/assign';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
-import SearchBar from './components/search_bar';
-import VideoList from './components/video_list';
-import VideoDetail from './components/video_detail';
-const API_KEY = 'AIzaSyDa83bQblKhl-St7djPDHj_lYn5ivQAbqg';
+import App from './components/Main';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      videos: [],
-      selectedVideo: null
-     };
-
-    this.videoSearch('banana pudding');
-  }
-
-  videoSearch(term) {
-    YTSearch({key: API_KEY, term: term}, (videos) => {
-      this.setState({
-        videos:videos,
-        selectedVideo: videos[0]
-       });
-    });
-  }
-
-  render() {
-    const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 500);
-
-    return (
-      <div>
-        <SearchBar onSearchTermChange={videoSearch} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
-          videos={this.state.videos} />
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.querySelector('.react-app'));
+// Render the main component into the dom
+ReactDOM.render(<App />, document.getElementById('app'));
